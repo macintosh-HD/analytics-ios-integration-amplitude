2e38f229cc291d3aac523beb7f5fc1e6469c5eb8
Julian Gentges <julgentges@gmail.com>
2021-01-25 11:01:10 +0100

    use relative import statements

--- Sources/SegmentAmplitude/SegmentAmplitude.h
+++ Sources/SegmentAmplitude/SegmentAmplitude.h
@@ -16,6 +16,6 @@ FOUNDATION_EXPORT const unsigned char SegmentAmplitudeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SegmentAmplitude/PublicHeader.h>
 
-#import <SegmentAmplitude/SEGAmplitudeIntegration.h>
-#import <SegmentAmplitude/SEGAmplitudeIntegrationFactory.h>
+#import "SEGAmplitudeIntegration.h"
+#import "SEGAmplitudeIntegrationFactory.h"
 
